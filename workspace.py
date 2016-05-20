b3e50f5207f7ef52f463d2369ceda67ceb16f5b3
Ilia Kurenkov <ilia.kurenkov@gmail.com>
2016-05-20 09:50:29 +0200

    Updated the workspace.

--- workspace.py
+++ workspace.py
@@ -3,14 +3,18 @@
 # every time in my notebook.
 # So I'm trying to solve this by providing a "namespace" of sorts
 
+import pickle
+import logging
 
 import numpy as np
 import pandas as pd
 
 # Theano stuff
+import theano
 from theano import tensor
 
 # All sorts of bricks
+from blocks.model import Model
 from blocks.bricks import Linear
 from blocks.bricks import NDimensionalSoftmax
 from blocks.bricks import Rectifier
@@ -20,6 +24,10 @@ from blocks.bricks.recurrent import SimpleRecurrent
 from blocks.bricks.sequences import Sequence
 from blocks.bricks.cost import CategoricalCrossEntropy
 from blocks.bricks.cost import MisclassificationRate
+from blocks.bricks.sequence_generators import (SequenceGenerator,
+                                               Readout,
+                                               SoftmaxEmitter,
+                                               LookupFeedback)
 
 # All sorts of blocks
 from blocks.graph import ComputationGraph
@@ -27,15 +35,20 @@ from blocks.initialization import IsotropicGaussian, Constant
 from blocks.algorithms import GradientDescent, Scale, Adam
 
 # Fuel Imports
-from fuel.datasets import Dataset
+from fuel.datasets import IndexableDataset, Dataset
 from fuel.streams import DataStream
 from fuel.schemes import SequentialScheme, SequentialExampleScheme
-from fuel.datasets import IndexableDataset
 
 from fuel.transformers import Padding, Mapping, Batch
 
 # Main Loop
-from blocks.extensions.monitoring import DataStreamMonitoring
 from blocks.main_loop import MainLoop
-from blocks.extensions import FinishAfter, Printing
+from blocks.extensions import FinishAfter, Printing, Timing
+from blocks.extensions.monitoring import DataStreamMonitoring
+from blocks.extensions.saveload import Checkpoint
 from blocks_extras.extensions.plot import Plot
+
+# (De)serialization
+from blocks.serialization import dump, load
+
+stream_log_handler = logging.StreamHandler()
